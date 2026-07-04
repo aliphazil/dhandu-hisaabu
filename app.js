cad3f8bc9a5304736028f8ea7217a168dad117ae
aliphazil <62470889+aliphazil@users.noreply.github.com>
2026-07-04 15:24:02 +0500

    Update island placeholder example from Thoddoo to H.Dh. Vaikaradhoo

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ import {
   recoverPassword,
   changePassword,
   pullFromFirestore
-} from './database.js?v=1.6.9';
+} from './database.js?v=1.7.0';
 
 // Global 2 decimal places number formatter
 function format2DP(val) {
